c4f80e545e5014fc865e76bef73acbc9cc3dca4d
Ethan Hayon <ehayon@gmail.com>
2011-08-21 22:05:21 -0400

    Initial Commit

---
+++ app/models/user.rb
@@ -0,0 +1,26 @@
+class User < ActiveRecord::Base
+  attr_accessible :email, :password, :password_confirmation
+  
+  attr_accessor :password, :password_confirmation
+  
+  validates_confirmation_of :password
+  validates_presence_of :password, :on => :create
+  validates_presence_of :email
+  
+  before_save :encrypt
+  
+  def encrypt
+    if password.present? 
+      self.password_salt = BCrypt::Engine.generate_salt()
+      self.password_hash = BCrypt::Engine.hash_secret(password, password_salt)
+    end
+  end
+  
+  def self.authenticate(email, password)
+    user = find_by_email(email)
+    if user && user.password_hash = BCrypt::Engine.hash_secret(password, user.password_salt)
+      user
+    end
+  end
+  
+end
---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+SimpleAuth::Application.config.secret_token = 'ff84cb3b74eb050538582589bcce7086c5fb5659c804938d19fc386cfb43c834445b3229613692dd69fbf40dd5ac228a610c6ce5c5f23852b6fc32c6c9a09a0e'
